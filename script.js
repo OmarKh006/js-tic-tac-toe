d4ffabe2d8e35e6d0aeb386dbae96c3d327bd903
OmarKh006 <khairyomar006@gmail.com>
2026-04-11 07:10:35 +0200

    keyboard accessibility

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const NUMBER_OF_ROWS = 4;
+const NUMBER_OF_ROWS = 3;
 const turns = NUMBER_OF_ROWS ** 2;
 const container = document.querySelector(".container");
 const resetButton = document.querySelector("#reset");
@@ -151,19 +151,29 @@ const cellClickHandler = (event, index) => {
   }
 };
 
+const createCell = (index) => {
+  const cellElementString = `<div class="cell" role="button" tabindex="${index + 1}"><span class="value"></span></div>`;
+
+  const cellElement = document
+    .createRange()
+    .createContextualFragment(cellElementString);
+
+  cellElement.querySelector(".cell").onclick = (event) =>
+    cellClickHandler(event, index);
+
+  cellElement.querySelector(".cell").onkeydown = (event) =>
+    event.key === "Enter" ? cellClickHandler(event, index) : true;
+
+  return cellElement;
+};
+
 const createBoard = () => {
   const board = document.createElement("div");
   board.classList.add("board");
 
   for (let i = 0; i < NUMBER_OF_ROWS ** 2; i++) {
-    const cellElementString = `<div class="cell"><span class="value"></span></div>`;
-    const cellElement = document
-      .createRange()
-      .createContextualFragment(cellElementString);
-    cellElement.querySelector(".cell").onclick = (event) =>
-      cellClickHandler(event, i);
+    const cellElement = createCell(i);
     board.appendChild(cellElement);
-
     document.documentElement.style.setProperty("--grid-rows", NUMBER_OF_ROWS);
   }
 
